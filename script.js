664959177316076e274b11141e97b390093dbc5d
jeffersonsantos30 <jeferson29santos@gmail.com>
2023-06-27 21:25:42 -0300

    parte lógica finalizada

---
+++ script.js
@@ -0,0 +1,60 @@
+const inputElement = document.querySelector("#input");
+const fromElement = document.querySelector("#from");
+const toElement = document.querySelector("#para");
+const outputElement = document.querySelector("#output");
+const convertButton = document.querySelector("#convert-btn");
+const messageElement = document.querySelector("#message");
+
+// função para converter as unidades
+
+function convert (){
+    const fromValue = fromElement.value;
+    const toValue = toElement.value;
+
+    if( fromValue === toValue){
+        outputElement.value = inputElement.value;
+        messageElement.textContent = "";
+        return;
+    }
+    
+let meters;
+    switch (fromValue) {
+        case "m":
+            meters =  inputElement.value;
+            break;
+        case "km":
+            meters =  inputElement.value * 1000;
+            break;
+        case "cm":
+            meters =  inputElement.value / 100;
+            break;
+        case "mm":
+            meters =  inputElement.value / 1000;
+            break;
+    
+    }
+let result;
+    switch (toValue) {
+        case "m":
+            result =  meters;
+            break;
+        case "km":
+            result =  meters /1000;
+            break;
+        case "cm":
+            result =  meters * 100;
+            break;
+        case "mm":
+            result =  meters * 1000;
+            break;
+    
+    }
+
+    outputElement.value = result;
+}
+
+ClickConvert();
+function ClickConvert() {
+    convertButton.addEventListener("click", convert);
+}
+
